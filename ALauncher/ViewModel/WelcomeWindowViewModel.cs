f19b9f5d5270d8bf2022b4d2fc1a3223a818196a
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-05 02:14:58 +0300

    Обновил WelcomeWindowViewModel

--- ALauncher/ViewModel/WelcomeWindowViewModel.cs
+++ ALauncher/ViewModel/WelcomeWindowViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ALauncher.ViewModel
 {
     internal class WelcomeWindowViewModel : BaseViewModel
     {
         private readonly Settings _settings;
+
+        public ICommand HyperlinkCommand { get; set; }
+        public ICommand BrowseBtnCommand { get; set; }
 
         public string ModAPIPath
         {
@@ -19,10 +24,57 @@ namespace ALauncher.ViewModel
                 OnPropertyChanged();
             }
         }
+        public bool? IsSteamVersion
+        {
+            get => _settings.IsSteamVersion;
+            set
+            {
+                _settings.IsSteamVersion = value ?? false;
+                OnPropertyChanged();
+            }
+        }
+        private bool _noModAPI = false;
+        public bool? NoModAPI
+        {
+            get => _noModAPI;
+            set
+            {
+                _noModAPI = value ?? false;
+                OnPropertyChanged();
+                ModAPIPathVisibility = _noModAPI ? Visibility.Collapsed :
+                    Visibility.Visible;
+            }
+        }
+        private Visibility _modAPIVisibility = Visibility.Visible;
+        public Visibility ModAPIPathVisibility
+        {
+            get => _modAPIVisibility;
+            private set
+            {
+                _modAPIVisibility = value;
+                OnPropertyChanged();
+            }
+        }
 
         public WelcomeWindowViewModel()
         {
             _settings = Settings.Instance;
+
+            HyperlinkCommand = new RelayCommand((uri) =>
+                System.Diagnostics.Process.Start("explorer.exe",
+                uri?.ToString() ?? "https://youtu.be/dQw4w9WgXcQ"));
+            BrowseBtnCommand = new RelayCommand(BrowseFolder);
+        }
+
+        private void BrowseFolder(object? obj)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderBrowser = new();
+            var result = folderBrowser.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.Cancel ||
+                string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+                return;
+
+            ModAPIPath = folderBrowser.SelectedPath;
         }
     }
 }
--- ALauncher/WelcomeWindow.xaml.cs
+++ ALauncher/WelcomeWindow.xaml.cs
@@ -16,42 +16,22 @@ namespace ALauncher
             _settings = Settings.Instance;
         }
 
-        private void Hyperlink_Click(object sender, RoutedEventArgs e) =>
-            System.Diagnostics.Process.Start("explorer.exe",
-                ((Hyperlink)sender).NavigateUri.ToString());
-
-        private void NoModAPICheck_Checked(object sender, RoutedEventArgs e) =>
-            ModAPIPathDock.Visibility = Visibility.Collapsed;
-
-        private void NoModAPICheck_Unchecked(object sender, RoutedEventArgs e) =>
-            ModAPIPathDock.Visibility= Visibility.Visible;
-
+        // Перенести в vm
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            bool noModAPI = NoModAPICheck.IsChecked ?? false;
-            if (string.IsNullOrWhiteSpace(ModAPIPathBox.Text) && !noModAPI)
-            {
-                MessageBox.Show("Введите путь до ModAPI", "Ошибка!",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+            //bool noModAPI = NoModAPICheck.IsChecked ?? false;
+            //if (string.IsNullOrWhiteSpace(ModAPIPathBox.Text) && !noModAPI)
+            //{
+            //    MessageBox.Show("Введите путь до ModAPI", "Ошибка!",
+            //        MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
 
-            _settings.ModAPIPath = ModAPIPathBox.Text;
+            //_settings.ModAPIPath = ModAPIPathBox.Text;
             _settings.IsFirstStart = false;
-            _settings.IsSteamVersion = IsSteamVersionCheck.IsChecked ?? false;
+            //_settings.IsSteamVersion = IsSteamVersionCheck.IsChecked ?? false;
             Settings.Serialize();
             Close();
-        }
-
-        private void BrowseBtn_Click(object sender, RoutedEventArgs e)
-        {
-            System.Windows.Forms.FolderBrowserDialog folderBrowser = new();
-            var result = folderBrowser.ShowDialog();
-            if (result == System.Windows.Forms.DialogResult.Cancel ||
-                string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
-                return;
-
-            ModAPIPathBox.Text = folderBrowser.SelectedPath;
         }
     }
 }

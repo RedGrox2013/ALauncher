0d0b710c56886513f912351f1ea4d88051ff2e7c
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-04 23:26:50 +0300

    Добавил WelcomeWindowViewModel

---
+++ ALauncher/ViewModel/WelcomeWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALauncher.ViewModel
+{
+    internal class WelcomeWindowViewModel : BaseViewModel
+    {
+        private readonly Settings _settings;
+
+        public string ModAPIPath
+        {
+            get => _settings.ModAPIPath;
+            set
+            {
+                _settings.ModAPIPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public WelcomeWindowViewModel()
+        {
+            _settings = Settings.Instance;
+        }
+    }
+}

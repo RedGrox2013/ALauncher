2dbb83bee4ac09ce2a81d6a9bae2df40336ccf56
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-05 22:17:27 +0300

    Обновил SettingsPage

--- ALauncher/View/SettingsPage.xaml.cs
+++ ALauncher/View/SettingsPage.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.IO;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace ALauncher.View
 {
@@ -16,32 +7,9 @@ namespace ALauncher.View
     /// </summary>
     public partial class SettingsPage : Page
     {
-        private readonly Settings _settings;
-
         public SettingsPage()
         {
             InitializeComponent();
-
-            _settings = Settings.Instance;
-            LineArgumentsBox.Text = _settings.LineArguments;
-        }
-
-        private void BackBtn_Click(object sender, RoutedEventArgs e) =>
-            NavigationService.GoBack();
-
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(ModAPIPathBox.Text))
-            {
-                DirectoryInfo? dir = new(ModAPIPathBox.Text);
-                if (dir.Name == Settings.MODAPI_NAME)
-                    dir = dir.Parent;
-                _settings.ModAPIPath = dir?.FullName ?? string.Empty;
-            }
-            _settings.LineArguments = LineArgumentsBox.Text;
-
-            Settings.Serialize();
-            BackBtn_Click(sender, e);
         }
     }
 }
--- ALauncher/ViewModel/SettingsViewModel.cs
+++ ALauncher/ViewModel/SettingsViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ALauncher.ViewModel
 {
     internal class SettingsViewModel : BaseViewModel
     {
         private readonly Settings _settings;
+
+        public ICommand SaveBtnCommand { get; private set; }
+        public ICommand GoBackCommand { get; private set; }
 
         private string _modAPIPath;
         public string ModAPIPath
@@ -20,11 +20,52 @@ namespace ALauncher.ViewModel
                 OnPropertyChanged();
             }
         }
+        private bool _isSteamVersion;
+        public bool? IsSteamVersion
+        {
+            get => _isSteamVersion;
+            set
+            {
+                _isSteamVersion = value ?? false;
+                OnPropertyChanged();
+            }
+        }
+        private string _lineArgs;
+        public string LineArguments
+        {
+            get => _lineArgs;
+            set
+            {
+                _lineArgs = value;
+                OnPropertyChanged();
+            }
+        }
 
         public SettingsViewModel()
         {
             _settings = Settings.Instance;
             _modAPIPath = _settings.ModAPIPath;
+            _isSteamVersion = _settings.IsSteamVersion;
+            _lineArgs = _settings.LineArguments;
+
+            SaveBtnCommand = new RelayCommand(SaveSettings);
+            GoBackCommand = new RelayCommand(GoBack);
+        }
+
+        private void GoBack(object? obj)
+        {
+            if (obj is Page page)
+                page.NavigationService.GoBack();
+        }
+
+        private void SaveSettings(object? obj)
+        {
+            _settings.ModAPIPath = _modAPIPath;
+            _settings.IsSteamVersion = _isSteamVersion;
+            _settings.LineArguments = _lineArgs;
+
+            Settings.Serialize();
+            GoBack(obj);
         }
     }
 }

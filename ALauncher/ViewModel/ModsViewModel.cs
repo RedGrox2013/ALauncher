203b368626c2422012e0e4e46d8c4e4425f78df5
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-11 16:09:11 +0300

    Добавил функциональность кнопкам на странице с модами

--- ALauncher/ViewModel/ModsViewModel.cs
+++ ALauncher/ViewModel/ModsViewModel.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ALauncher.ViewModel
 {
     internal class ModsViewModel : BaseViewModel
     {
+        private readonly Settings _settings;
+
+        public ICommand StartModsManagerCommand { get; private set; }
+
+        public ModsViewModel()
+        {
+            _settings = Settings.Instance;
+
+            StartModsManagerCommand = new RelayCommand(ModAPIKitStart);
+        }
+
+        private void ModAPIKitStart(object? fileName)
+        {
+            if (fileName == null)
+                throw new Exception("Не указан аргумент");
+
+            try
+            {
+                if (_settings.ModAPIPath[^1] != '\\' || _settings.ModAPIPath[^1] != '/')
+                    _settings.ModAPIPath += "\\";
+                Process.Start(_settings.ModAPIPath + fileName?.ToString());
+            }
+            catch
+            {
+                MessageBox.Show(
+                            "Пожалуйста, укажите путь до SporeModAPI. " +
+                            "Если у вас не установлен Spore ModAPI Launcher, " +
+                            "вы можете сделать это в открывшемся окне", "Проверьте настройки",
+                            MessageBoxButton.OK, MessageBoxImage.Error);
+                Process.Start("explorer.exe",
+                    "http://davoonline.com/sporemodder/rob55rod/ModAPI/Public/index.html");
+            }
+        }
     }
 }

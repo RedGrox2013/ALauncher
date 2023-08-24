fab27f3baa81e85b83a24d8c42a365be58be1889
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-24 13:45:25 +0300

    Добавил команды кнопкам

--- ALauncher/ViewModel/ModsViewModel.cs
+++ ALauncher/ViewModel/ModsViewModel.cs
@@ -10,12 +10,28 @@ namespace ALauncher.ViewModel
         private readonly Settings _settings;
 
         public ICommand StartModsManagerCommand { get; private set; }
+        public ICommand InstallModCommand { get; private set; }
+        public ICommand UninstallModCommand { get; private set; }
+
+        private const string MODAPI_URL = "http://davoonline.com/sporemodder/rob55rod/ModAPI/Public/index.html";
 
         public ModsViewModel()
         {
             _settings = Settings.Instance;
 
             StartModsManagerCommand = new RelayCommand(ModAPIKitStart);
+            InstallModCommand = new RelayCommand(UninstallMod);
+            UninstallModCommand = new RelayCommand(UninstallMod);
+        }
+
+        private void UninstallMod(object? obj)
+        {
+            if (string.IsNullOrWhiteSpace(_settings.ModAPIPath))
+            {
+                LauncherMessageBox.ShowModAPIError();
+                Process.Start("explorer.exe", MODAPI_URL);
+                return;
+            }
         }
 
         private void ModAPIKitStart(object? fileName)
@@ -30,8 +46,7 @@ namespace ALauncher.ViewModel
             catch
             {
                 LauncherMessageBox.ShowModAPIError();
-                Process.Start("explorer.exe",
-                    "http://davoonline.com/sporemodder/rob55rod/ModAPI/Public/index.html");
+                Process.Start("explorer.exe", MODAPI_URL);
             }
         }
     }

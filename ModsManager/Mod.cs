86aec8adc182927987626ff8602a61b9dd7bfe5d
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-27 22:46:58 +0300

    Добавил новые проекты

---
+++ ModsManager/Mod.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml.Serialization;
+
+namespace ModsManager
+{
+    [Serializable, XmlRoot("mod")]
+    public class Mod
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        [XmlAttribute("displayName")]
+        public string DisplayName { get; set; }
+        [XmlAttribute("unique")]
+        public string Unique { get; set; }
+        [XmlAttribute("description")]
+        public string? Description { get; set; }
+        [XmlAttribute("installerSystemVersion")]
+        public string InstallerSystemVersion { get; set; }
+        [XmlAttribute("hasCustomInstaller")]
+        public bool HasCustomInstaller { get; set; }
+        [XmlAttribute("dllsBuild")]
+        public string DllsBuild { get; set; }
+        [XmlAttribute("isExperimental")]
+        public bool IsExperimental { get; set; }
+        [XmlAttribute("requiresGalaxyReset")]
+        public bool RequiresGalaxyReset { get; set; }
+        [XmlAttribute("causesSaveDataDependency")]
+        public bool CausesSaveDataDependency { get; set; }
+
+        public Mod() : this("unknown", "unknown", "1.0.1.0", "2.5.20") { }
+        public Mod(string name, string unique, string installerSystemVersion,
+            string dllsBuild, string? description = null, bool hasCustomInstaller = false,
+            bool isExperimental = false, bool requiresGalaxyReset = false,
+            bool causesSaveDataDependency = false)
+        {
+            Name = DisplayName = name;
+            Unique = unique;
+            Description = description;
+            InstallerSystemVersion = installerSystemVersion;
+            HasCustomInstaller = hasCustomInstaller;
+            DllsBuild = dllsBuild;
+            IsExperimental = isExperimental;
+            RequiresGalaxyReset = requiresGalaxyReset;
+            CausesSaveDataDependency = causesSaveDataDependency;
+        }
+
+        public override string ToString()
+            => $@"{nameof(Name)}: {Name}
+{nameof(DisplayName)}: {DisplayName}
+{nameof(Unique)}: {Unique}
+{nameof(Description)}: {Description}
+{nameof(InstallerSystemVersion)}: {InstallerSystemVersion}
+{nameof(HasCustomInstaller)}: {HasCustomInstaller}
+{nameof(DllsBuild)}: {DllsBuild}
+{nameof(IsExperimental)}: {IsExperimental}
+{nameof(RequiresGalaxyReset)}: {RequiresGalaxyReset}
+{nameof(CausesSaveDataDependency)}: {CausesSaveDataDependency}";
+    }
+}
---
+++ ModsManagerTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using ModsManager;
+using System.Xml.Serialization;
+
+XmlSerializer xmlSerializer = new(typeof(Mod));
+Console.Write("Serialize or deserialize (s/d)? ");
+var input = Console.ReadLine();
+string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/test.xml";
+if (input == "s")
+{
+    Mod mod = new();
+    using var stream = File.Create(path);
+    xmlSerializer.Serialize(stream, mod);
+}
+else if (input == "d")
+{
+    using var stream = File.OpenRead(path);
+    if (xmlSerializer.Deserialize(stream) is Mod mod)
+        Console.WriteLine(mod);
+}

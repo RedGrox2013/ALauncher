53b61ec28be72f869df530500be7db4a19f5906e
Барсик Рыжий <ryzijbarsik95@gmail.com>
2023-08-29 10:11:39 +0300

    Переделал менеджер модов

--- ModsManager/Mod.cs
+++ ModsManager/Mod.cs
@@ -1,61 +1,33 @@
-﻿using System.Xml.Serialization;
+﻿using System.Xml;
+using System.Xml.Serialization;
 
 namespace ModsManager
 {
-    [Serializable, XmlRoot("mod")]
     public class Mod
     {
-        [XmlAttribute("name")]
-        public string Name { get; set; }
-        [XmlAttribute("displayName")]
-        public string DisplayName { get; set; }
-        [XmlAttribute("unique")]
-        public string Unique { get; set; }
-        [XmlAttribute("description")]
-        public string? Description { get; set; }
-        [XmlAttribute("installerSystemVersion")]
-        public string InstallerSystemVersion { get; set; }
-        [XmlAttribute("hasCustomInstaller")]
-        public bool HasCustomInstaller { get; set; }
-        [XmlAttribute("dllsBuild")]
-        public string DllsBuild { get; set; }
-        [XmlAttribute("isExperimental")]
-        public bool IsExperimental { get; set; }
-        [XmlAttribute("requiresGalaxyReset")]
-        public bool RequiresGalaxyReset { get; set; }
-        [XmlAttribute("causesSaveDataDependency")]
-        public bool CausesSaveDataDependency { get; set; }
+        public string? Name { get; set; }
+        public string? DisplayName { get; set; }
 
-        [XmlElement("prerequisite")]
-        public ModPrerequisite Prerequisite { get; set; }
-
-        public Mod() : this("unknown", "unknown", "1.0.1.0", "2.5.20") { }
-        public Mod(string name, string unique, string installerSystemVersion,
-            string dllsBuild, string? description = null, bool hasCustomInstaller = false,
-            bool isExperimental = false, bool requiresGalaxyReset = false,
-            bool causesSaveDataDependency = false)
+        public static Mod ParseXML(string path)
         {
-            Name = DisplayName = name;
-            Unique = unique;
-            Description = description;
-            InstallerSystemVersion = installerSystemVersion;
-            HasCustomInstaller = hasCustomInstaller;
-            DllsBuild = dllsBuild;
-            IsExperimental = isExperimental;
-            RequiresGalaxyReset = requiresGalaxyReset;
-            CausesSaveDataDependency = causesSaveDataDependency;
-        }
+            Mod mod = new();
+            using XmlReader reader = XmlReader.Create(path);
+            while (!reader.EOF)
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "mod")
+                    {
+                        mod.Name = reader.GetAttribute("name");
+                        mod.DisplayName = reader.GetAttribute("displayName") ?? mod.Name;
+                    }
+                    reader.Read();
+                }
+                else
+                    reader.Read();
+            }
 
-        public override string ToString()
-            => $@"{nameof(Name)}: {Name}
-{nameof(DisplayName)}: {DisplayName}
-{nameof(Unique)}: {Unique}
-{nameof(Description)}: {Description}
-{nameof(InstallerSystemVersion)}: {InstallerSystemVersion}
-{nameof(HasCustomInstaller)}: {HasCustomInstaller}
-{nameof(DllsBuild)}: {DllsBuild}
-{nameof(IsExperimental)}: {IsExperimental}
-{nameof(RequiresGalaxyReset)}: {RequiresGalaxyReset}
-{nameof(CausesSaveDataDependency)}: {CausesSaveDataDependency}";
+            return mod;
+        }
     }
 }
--- ModsManagerTest/Program.cs
+++ ModsManagerTest/Program.cs
@@ -1,26 +1,5 @@
 ﻿using ModsManager;
-using System.Xml.Serialization;
 
-XmlSerializer xmlSerializer = new(typeof(Mod));
-Console.Write("Serialize or deserialize (s/d)? ");
-var input = Console.ReadLine();
 string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/test.xml";
-if (input == "s")
-{
-    Mod mod = new()
-    {
-        Prerequisite = new ModPrerequisite()
-        {
-            File = "test",
-            Game = "Spore"
-        }
-    };
-    using var stream = File.Create(path);
-    xmlSerializer.Serialize(stream, mod);
-}
-else if (input == "d")
-{
-    using var stream = File.OpenRead(path);
-    if (xmlSerializer.Deserialize(stream) is Mod mod)
-        Console.WriteLine(mod);
-}
+var mod = Mod.ParseXML(path);
+Console.WriteLine(mod.Name + " " + mod.DisplayName);
